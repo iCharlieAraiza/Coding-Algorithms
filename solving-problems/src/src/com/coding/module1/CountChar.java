824c7648d6cc5ba20d10e0dcc8b166dd57b01b62
Carlos Araiza <eliptikman1234@gmail.com>
2021-05-17 22:45:07 -0500

    Add new repository

--- solving-problems/src/src/com/coding/module1/CountChar.java
+++ solving-problems/src/src/com/coding/module1/CountChar.java
@@ -1,4 +1,23 @@
 package com.coding.module1;
 
+import java.util.HashMap;
+
 public class CountChar {
+
+    public HashMap<Character,Integer> count(String word){
+
+        HashMap<Character, Integer> result = new HashMap<>();
+        int length = word.length();
+
+        for(int i = 0; i<length; i++){
+            char letter = word.charAt(i);
+            if(result.containsKey(letter)){
+                int val = result.get(letter);
+                result.put(letter, ++val);
+            }else{
+                result.put(letter, 1);
+            }
+        }
+        return result;
+    }
 }
---
+++ solving-problems/src/src/com/coding/test/CountCharTest.java
@@ -0,0 +1,28 @@
+package com.coding.test;
+
+import com.coding.module1.CountChar;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CountCharTest {
+
+    //This should return
+    @Test
+    void fourAExample(){
+        CountChar counter = new CountChar();
+        HashMap<Character, Integer> result = counter.count("abasasas");
+        assertEquals("{a=4, b=1, s=3}", result.toString());
+    }
+
+    @Test
+    void testHello(){
+        CountChar counter = new CountChar();
+        HashMap<Character, Integer> result = counter.count("hello");
+        assertEquals("{e=1, h=1, l=2, o=1}", result.toString());
+    }
+
+}
