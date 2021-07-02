c7c6286de3c09e005e6755ba44bb1dab53c57691
iCharlieAraiza <eliptikman1234@gmail.com>
2021-07-02 04:19:25 -0500

    Update functional programming part

---
+++ solving-problems/functional programming/src/org/course/_Function.java
@@ -0,0 +1,10 @@
+package org.course;
+
+import java.util.function.Function;
+
+public class _Function {
+
+    Function<Integer, Integer> incrementByOne = number -> number + 1;
+
+    Function<Integer, Integer> multiplyByTen = number -> number * 10;
+}
---
+++ solving-problems/functional programming/src/org/course/_FunctionTest.java
@@ -0,0 +1,32 @@
+package org.course;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.function.Function;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class _FunctionTest {
+    private _Function test;
+    @BeforeEach
+    void beforeEach(){
+        test = new _Function();
+    }
+
+    @Test
+    void addOne(){
+        assertEquals( 2, test.incrementByOne.apply(1) );
+    }
+
+    @Test
+    void multiplyByTen(){
+        assertEquals( 20, test.multiplyByTen.apply(2) );
+    }
+
+    @Test
+    void addOneAndThenMultiplyByTen(){
+        Function<Integer, Integer> addOneAndThenMultiplyByTen = test.incrementByOne.andThen( test.multiplyByTen );
+        assertEquals(20, addOneAndThenMultiplyByTen.apply(1) );
+    }
+}
