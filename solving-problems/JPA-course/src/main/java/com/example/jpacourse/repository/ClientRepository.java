ebc1dee81019cf67192e210e51abd017f380f5be
iCharlieAraiza <eliptikman1234@gmail.com>
2021-07-12 18:49:44 -0500

    Init commit

---
+++ solving-problems/JPA-course/src/main/java/com/example/jpacourse/repository/ClientRepository.java
@@ -0,0 +1,9 @@
+package com.example.jpacourse.repository;
+
+import com.example.jpacourse.model.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long> {
+}
