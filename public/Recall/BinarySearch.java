6ac070a7389049215e782c01f352fc94d00bda1c
iCharlieAraiza <eliptikman1234@gmail.com>
2021-11-05 04:17:18 -0600

    Update BinarySearch.java

--- public/Recall/BinarySearch.java
+++ public/Recall/BinarySearch.java
@@ -5,29 +5,25 @@ public class BinarySearch {
     public static void main(String args[]) {
         //Time complexity O(log n)
         //Test List= [-2, 0, 1, 4, 8, 10, 11, 20]   Targer= [10] Result = 5
-        int[] list = new int[]{2, 5, 6, 9, 13, 15, 28, 30};
-        int target = 30;
+        int[] list = new int[]{2, 5, 6, 9, 13, 15, 28, 30, 33, 34, 35};
+        int target = 9;
         
         System.out.println( getIndex(list, target) );
     }
     
     public static int getIndex(int[] list, int target){
         int p = 0,
-            q = list.length,
-            midPoint = (int)Math.floor((p + q) / 2);
-
-        while(p <= q && list[midPoint]!=target){
-            midPoint = (int)Math.floor((p + q) / 2);
-            if(list[midPoint] == target){
-                return midPoint;
-            }else{
-                if( list[midPoint] < target ){
-                    p = midPoint+1;
-                }else{
-                    q = midPoint-1;
-                }
+            q = list.length, 
+            midPoint;
+        do{
+            midPoint = (int) Math.floor((p + q) / 2);
+            //System.out.println(p + " " + +midPoint+ " " + q);
+            if( list[midPoint] == target) return midPoint;
+            else{
+                if( list[midPoint] < target ) p = midPoint+1;
+                else q = midPoint-1;
             }
-        }
+        }while(p < q);
         return -1;
     }
 }
