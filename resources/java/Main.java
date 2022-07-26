4d7a8823ef04b7edaf5883418ffd3cde0e36342a
iCharlieAraiza <eliptikman1234@gmail.com>
2022-07-25 23:55:47 -0500

    Update Main.java

--- resources/java/Main.java
+++ resources/java/Main.java
@@ -3,4 +3,9 @@ class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
+    //Declare an array
+    public static void declareArray() {
+        int[] array = new int[10];
+    }
+
 }
