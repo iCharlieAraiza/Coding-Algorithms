c3f5d073e67490b1e57b203dcea98442ec179350
Carlos Araiza <eliptikman1234@gmail.com>
2023-10-14 23:49:03 -0500

    Create stackProblems.java

---
+++ datastructures/stackProblems.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+
+        int goodIndex = 0;
+
+        for (int i = nums.length - 1; i >= 0; i++) {        
+            if(i + nums.length[i] > goodIndex) {
+                goodOIndex = 
+            }
+        }
+
+        return true;
+    }
+
+
+    /*
+
+        2,3,1,1,4
+            0
+    
+     */
+
+}
